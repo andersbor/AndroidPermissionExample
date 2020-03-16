45c9be5fcbe42e304944c5cb6d984e09a7c873a7
Anders Børjesson <anbo@zealand.dk>
2020-03-16 21:01:02 +0100

    more messages

--- app/src/main/java/dk/easj/anbo/permissionexample/MainActivity.java
+++ app/src/main/java/dk/easj/anbo/permissionexample/MainActivity.java
@@ -7,17 +7,21 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSION_REQUEST_CODE = 1234;
     private static final String LOG_TAG = "MINE";
+    private TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        messageView = findViewById(R.id.mainMessageTextView);
     }
 
     // Head First, 2nd, page 813
@@ -27,11 +31,13 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             Log.d(LOG_TAG, "Must ask for permission");
+            messageView.setText("Must ask for permission");
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     MY_PERMISSION_REQUEST_CODE);
         } else {
             Log.d(LOG_TAG, "Permission already given");
+            messageView.setText("Permission already given");
         }
     }
 
@@ -41,8 +47,10 @@ public class MainActivity extends AppCompatActivity {
             case MY_PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d(LOG_TAG, "permission given");
+                    messageView.setText("permission given");
                 } else {
                     Log.d(LOG_TAG, "Permission not given");
+                    messageView.setText("Permission not given");
                 }
         }
     }
